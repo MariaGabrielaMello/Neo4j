ec5c8c980f716e0580239a67afba0a1bf867e4f8
Maria Gabriela Mello de Oliveira <127119599+MariaGabrielaMello@users.noreply.github.com>
2024-06-27 06:40:41 -0300

    Update connect_database.py

--- connect_database.py
+++ connect_database.py
@@ -1,8 +1,8 @@
 from neo4j import GraphDatabase
 
-uri = "neo4j+s://a9bfa888.databases.neo4j.io"
+uri = "neo4j+s://"
 username = "neo4j"
-password = "uygzFjyQ_DbUsgSPf_P4ywowk4eVbv4r5uOf1GiIafo"
+password = "senha-aqui"
 
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
